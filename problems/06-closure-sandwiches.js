c6b30a7a21cc47a711ef1c00c8a497b101d5636b
SpiritGun91 <spiritgun@vivaldi.net>
2025-04-17 20:19:30 -0400

    Returns a function that accepts new arguments for the order.

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
-This function will return another function that will accept a string to add 
+This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
@@ -18,12 +18,18 @@ sandwich2("pb") // => "One sandwich with tomato and pb"
 ***********************************************************************/
 
 function sandwichMaker() {
-  // Your code here
+	// Your code here
+	let order = "One sandwich with tomato";
+
+	return (ingredient) => {
+		order += ` and ${ingredient}`;
+		return order;
+	};
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
-  module.exports = sandwichMaker;
+	module.exports = sandwichMaker;
 } catch (e) {
-  return null;
+	return null;
 }
